78b5ad2bb839c68256d06934256de5d7c23d9701
gclement <58073759+gambas-maker@users.noreply.github.com>
2023-09-04 20:04:32 +0200

    Update forms.py

--- focus_pomo/accounts/forms.py
+++ focus_pomo/accounts/forms.py
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-from typing import Any
-from django import forms
-from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
-
-class CustomUserCreationForm(UserCreationForm):
-
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'email')
-
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-
-        for fieldname in ['password1', 'password2']:
-            self.fields[fieldname].help_text = None
-
-class CustomUserChangeForm(UserChangeForm):
-
-    class Meta:
-        model = CustomUser
-        fields = ('username', 'email')
-=======
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
@@ -36,4 +11,3 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('email', 'password')
->>>>>>> 6f35e699ff10e261826416ea7ca6d108e01df272
