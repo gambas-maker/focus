38dd0a1c56a73f01009340fd4aee20db12ed6dd8
gclement <58073759+gambas-maker@users.noreply.github.com>
2023-09-04 20:03:29 +0200

    Update tests.py

--- focus_pomo/accounts/tests.py
+++ focus_pomo/accounts/tests.py
@@ -1,47 +1,11 @@
-<<<<<<< HEAD
-from django.test import TestCase
+from django.urls import path,include
+from . import views
+from django.contrib.auth import views as auth_views
 
-# Create your tests here.
-=======
-from django.contrib.auth import get_user_model
-from django.test import TestCase
+app_name = 'accounts'
 
-# Create your tests here.
-class UsersManagersTest(TestCase):
-    def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(email="normal@user.com", password="foo")
-        self.assertEqual(user.email, "normal@user.com")
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
-        try:
-            # username is None for the AbstractUser option
-            # username does not exist for the AbstractBaseUser option
-            self.assertIsNone(user.username)
-        except AttributeError:
-            pass
-        with self.assertRaises(TypeError):
-            User.objects.create_user()
-        with self.assertRaises(TypeError):
-            User.objects.create_user(email="")
-        with self.assertRaises(ValueError):
-            User.objects.create_user(email="", password="foo")
-
-    def test_create_superuser(self):
-        User = get_user_model()
-        admin_user = User.objects.create_superuser(email="super@user.com", password="foo")
-        self.assertEqual(admin_user.email, "super@user.com")
-        self.assertTrue(admin_user.is_active)
-        self.assertTrue(admin_user.is_staff)
-        self.assertTrue(admin_user.is_superuser)
-        try:
-            # username is None for the AbstractUser option
-            # username does not exist for the AbstractBaseUser option
-            self.assertIsNone(admin_user.username)
-        except AttributeError:
-            pass
-        with self.assertRaises(ValueError):
-            User.objects.create_superuser(
-                email="super@user.com", password="foo", is_superuser=False)
->>>>>>> 6f35e699ff10e261826416ea7ca6d108e01df272
+urlpatterns = [
+    path('sign_up/', views.SignUp.as_view(), name='sign_up'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
