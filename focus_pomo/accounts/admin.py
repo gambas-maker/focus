c4966770907af9d94268aa4068d025502013dcc7
gclement <58073759+gambas-maker@users.noreply.github.com>
2023-09-04 20:05:48 +0200

    Update admin.py

--- focus_pomo/accounts/admin.py
+++ focus_pomo/accounts/admin.py
@@ -1,26 +1,16 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from django.contrib.auth import get_user_model
-=======
 
 # Register your models here.
->>>>>>> 6f35e699ff10e261826416ea7ca6d108e01df272
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 6f35e699ff10e261826416ea7ca6d108e01df272
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-<<<<<<< HEAD
-    list_display = ['email', 'username',]
-=======
     list_display = ("email", "is_staff", "is_active",)
     list_filter = ("email", "is_staff", "is_active",)
     fieldsets = (
@@ -39,6 +29,5 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ("email",)
     ordering = ("email",)
 
->>>>>>> 6f35e699ff10e261826416ea7ca6d108e01df272
 
 admin.site.register(CustomUser, CustomUserAdmin)
