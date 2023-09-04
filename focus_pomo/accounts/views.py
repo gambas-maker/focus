3f7d3dee1ca90f25e3e8861a82ecd5c9f80e0fe5
gclement <58073759+gambas-maker@users.noreply.github.com>
2023-09-04 20:02:47 +0200

    Update views.py

--- focus_pomo/accounts/views.py
+++ focus_pomo/accounts/views.py
@@ -1,16 +1,4 @@
 from django.shortcuts import render
-<<<<<<< HEAD
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from . import forms
-
-# Create your views here.
-class SignUp(CreateView):
-
-    form_class = forms.CustomUserCreationForm
-    success_url = reverse_lazy('index.html')
-    template_name = 'accounts/sign_up.html'
-=======
 from django.views.generic import CreateView
 from django.contrib.auth import get_user_model
 from . import models
@@ -25,4 +13,3 @@ class SignUp(CreateView):
     model = User
     template_name = 'accounts/sign_up.html'
     success_url = reverse_lazy('accounts:login') 
->>>>>>> 6f35e699ff10e261826416ea7ca6d108e01df272
