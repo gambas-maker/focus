587719c87b3a00dee3db3f2e1d2ebbc64e8ccea7
gambas-maker <clement.guibaud@protonmail.com>
2023-10-17 22:15:25 +0200

    update task creation widget, WIP

--- focus_pomo/tasks/forms.py
+++ focus_pomo/tasks/forms.py
@@ -1,7 +1,24 @@
 from tasks.models import Task
 from django import forms
+from django.forms import TextInput
 
 class TaskForm(forms.ModelForm):
     class Meta():
         model = Task
         fields = ('title', 'note', 'rounds')
+        widgets = {
+            'title': TextInput(attrs={
+                'class': "form-control",
+                'placeholder': 'What are you working on?',
+            }),
+            'note': TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Some notes...",
+            })
+        }
+        labels = {
+            'title': "",
+            'note': "",
+            'rounds': ""
+
+        }
